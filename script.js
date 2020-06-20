930d4f92f9d1ee5d005d8469480cb313c01b5587
Ayla Dillis <aayladillis@gmail.com>
2020-06-20 09:56:23 -0700

    coding start button with html/css

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 
 var timeEl = document.querySelector(".time");
+
 var addBtn = document.querySelector("#add-btn");
-var peopleListEl = document.querySelector("#people-list");
-var initialEl = document.querySelector("#initial");
+var initialsListEl = document.querySelector("#people-list");
+var initialsEl = document.querySelector("#initial");
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-var secondsLeft = 10;
-var people = [{ name: "Bob"}];
+var secondsLeft = 75;
+
+var initialsEl = [{ name: " "}];
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
