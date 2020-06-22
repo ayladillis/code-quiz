b308f87aaa64ae2289936a509b0a9494aa11f10f
Ayla Dillis <aayladillis@gmail.com>
2020-06-22 07:48:47 -0700

    adding question run through

--- script.js
+++ script.js
@@ -1,92 +1,156 @@
 
-var timeEl = document.querySelector("timer");
+// select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
 
-let startButton = document.getElementById("#start-button");
-let startScreen = document.getElementById("#start-screen");
-let quiz = document.getElementById("#quiz");
-let question = document.getElementById("#question");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
 
-let choiceA = document.getElementById("#A");
-let choiceB = document.getElementById("#B");
-let choiceC = document.getElementById("#C");
+const counter = document.getElementById("counter");
 
-var secondsLeft = 75;
+const time = document.getElementById("time");
 
-var initialsEl = [{ name: " "}];
+const progress = document.getElementById("progress");
 
-//Timer
+const scoreDiv = document.getElementById("score");
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = secondsLeft + "seconds remaining";
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 7500);
-}
-
-//Start button
-startButton.addEventListener("click", startQuiz);
-
-function startQuiz() {
-
-}
-
-
-// startButton.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     startScreen.setAttribute("style", "display: none;");
-//     question1.setAttribute("style", "display: block");
-// })
-
-//Quiz Questions
-
+// create questions
 let questions = [
     {
-        question: "what does HTML stand for?",
-        choiceA: "hyper text markup language",
-        choiceB: "home tool markup langauge",
-        choiceC: "hyperlink tool makeup language",
-        correct: "A"
-    },
-    {
-       question: "what does CSS stand for?" ,
-       choiceA: "consistent styling sheets",
-       choiceB: "cascading style sheets",
-       choiceC: "choosing style sheets",
-       correct: "B"
-    },
-    {
-        question: "what are the three main languages of the internet?",
-        choiceA: "HTML, CSS, Javascript",
-        choiceB: "HTML, CSS, Python",
-        choiceC: "HTML, Javascript, Python",
-        correct: "A"
-    },
-    {
-        question: "which site can you create repositories on?",
-        choiceA: "GitLink",
-        choiceB: "GitLab",
-        choiceC: "GitHub",
-        correct: "C"
-    },
-    {
-        question: "what is Jerome's favorite animal?",
-        choiceA: "cat",
-        choiceB: "dog",
-        choiceC: "guinea pig",
-        correct: "A"
+        question : "what does HTML stand for?",
+        choiceA : "hyper text markup language",
+        choiceB : "home tool markup langauge",
+        choiceC : "hyperlink tool makeup language",
+        correct : "A"
+    },{
+       question : "what does CSS stand for?" ,
+       choiceA : "consistent styling sheets",
+       choiceB : "cascading style sheets",
+       choiceC : "choosing style sheets",
+       correct : "B"
+    },{
+        question : "what are the three main languages of the internet?",
+        choiceA : "HTML, CSS, Javascript",
+        choiceB : "HTML, CSS, Python",
+        choiceC : "HTML, Javascript, Python",
+        correct : "A"
+    },{
+        question : "which site can you create repositories on?",
+        choiceA : "GitLink",
+        choiceB : "GitLab",
+        choiceC : "GitHub",
+        correct : "C"
+    },{
+        question : "what is Jerome's favorite animal?",
+        choiceA : "cat",
+        choiceB : "dog",
+        choiceC : "guinea pig",
+        correct : "A"
     }
 ];
+// assign variables 
 
-//to access questions
-questions[0].question
-questions[0].choiceA
-questions[0].choiceB
-questions[0].choiceC
-questions[0].correct
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 75; 
+let TIMER;
+let score = 0;
+
+// function to make questions appear 
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+  
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.addEventListener("click",startQuiz);
+
+// start quiz
+function startQuiz(){
+    start.style.display = "none";
+    textbox.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+}
+
+// move through questions
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+    }
+}
+
+//time
 
 
- 
+
+// score
+
+function renderCounter(){
+    if(count <= time){
+        counter.innerHTML = count;
+        count++
+    }else{
+        count = 0;
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            // end the quiz and show the score
+            clearInterval(TIMER);
+            scoreRender();
+        }
+    }
+}
+
+// checkAnwer
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is right
+        score++;
+       
+    }else{
+        // answer is wrong
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(TIMER);
+        scoreRender();
+    }
+}
+
+//answer is correct
+function answerIsCorrect(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+}
+
+//answer is Wrong
+function answerIsWrong(){
+    document.getElementById(runningQuestion).style.backgroundColor = "#f00";
+}
+
+// score render
+function scoreRender(){
+    scoreDiv.style.display = "block";
+    
+// calculate the questions correct
+const scorePerCent = Math.round(100 * score/questions.length);
+    
+}
+
+
