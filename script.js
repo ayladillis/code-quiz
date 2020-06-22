f3231e794dd5b0e8cfd5780f070d69280aec85fe
Ayla Dillis <aayladillis@gmail.com>
2020-06-21 21:06:00 -0700

    setting up quiz questions

--- script.js
+++ script.js
@@ -1,29 +1,92 @@
 
-var timeEl = document.querySelector(".time");
+var timeEl = document.querySelector("timer");
 
-let startButton = document.querySelector("#start-button");
+let startButton = document.getElementById("#start-button");
 let startScreen = document.getElementById("#start-screen");
-let question1 = document.getElementById("questions1");
+let quiz = document.getElementById("#quiz");
+let question = document.getElementById("#question");
 
-// var addBtn = document.querySelector("#add-btn");
-// var initialsListEl = document.querySelector("#people-list");
-// var initialsEl = document.querySelector("#initial");
-
-// const quizContainer = document.getElementById('quiz');
-// const resultsContainer = document.getElementById('results');
-// const submitButton = document.getElementById('submit');
+let choiceA = document.getElementById("#A");
+let choiceB = document.getElementById("#B");
+let choiceC = document.getElementById("#C");
 
 var secondsLeft = 75;
 
 var initialsEl = [{ name: " "}];
 
-//Start button
-startButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    startScreen.setAttribute("style", "display: none;");
-    question1.setAttribute("style", "display: block");
-})
+//Timer
 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = secondsLeft + "seconds remaining";
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 7500);
+}
+
+//Start button
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+
+}
+
+
+// startButton.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     startScreen.setAttribute("style", "display: none;");
+//     question1.setAttribute("style", "display: block");
+// })
+
+//Quiz Questions
+
+let questions = [
+    {
+        question: "what does HTML stand for?",
+        choiceA: "hyper text markup language",
+        choiceB: "home tool markup langauge",
+        choiceC: "hyperlink tool makeup language",
+        correct: "A"
+    },
+    {
+       question: "what does CSS stand for?" ,
+       choiceA: "consistent styling sheets",
+       choiceB: "cascading style sheets",
+       choiceC: "choosing style sheets",
+       correct: "B"
+    },
+    {
+        question: "what are the three main languages of the internet?",
+        choiceA: "HTML, CSS, Javascript",
+        choiceB: "HTML, CSS, Python",
+        choiceC: "HTML, Javascript, Python",
+        correct: "A"
+    },
+    {
+        question: "which site can you create repositories on?",
+        choiceA: "GitLink",
+        choiceB: "GitLab",
+        choiceC: "GitHub",
+        correct: "C"
+    },
+    {
+        question: "what is Jerome's favorite animal?",
+        choiceA: "cat",
+        choiceB: "dog",
+        choiceC: "guinea pig",
+        correct: "A"
+    }
+];
+
+//to access questions
+questions[0].question
+questions[0].choiceA
+questions[0].choiceB
+questions[0].choiceC
+questions[0].correct
 
 
  
